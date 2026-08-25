f4fb90921c1384852b2038922a3b36db35ba0b3d
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-25 12:15:18 +0200

    added a frame

--- src/main/java/org/java26/Main.java
+++ src/main/java/org/java26/Main.java
@@ -2,10 +2,8 @@ package org.java26;
 
 public class Main {
   public static void main (String[] args){
-
-
-      System.out.println("Hej världen!");
-
-
-    }
+      System.out.println("+----------------+");
+      System.out.println("|  Hej världen!  |");
+      System.out.println("+----------------+");
+  }
 }
